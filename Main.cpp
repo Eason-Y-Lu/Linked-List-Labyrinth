0b27cbf03976c1a38c5789a80f2ad98f894fec90
Eason Lu <ghcodesign@outlook.com>
2023-10-19 21:21:48 -0700

    12->13

--- Main.cpp
+++ Main.cpp
@@ -31,7 +31,7 @@ void generateCombinations(const std::string &characters, int length, std::string
 
 int main()
 {
-    generateCombinations("NSEW", 12, "");
+    generateCombinations("NSEW", 13, "");
     return 0;
 }
 // outfile << "This text will be appended to the file." << std::endl;
