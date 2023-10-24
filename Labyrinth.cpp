b01279c95bc7855a317cd9d9e4c637ed03f08f1b
Eason Lu <aelnosu@outlook.com>
2023-10-24 15:19:27 -0700

    update main and laby

--- Labyrinth.cpp
+++ Labyrinth.cpp
@@ -1,8 +1,42 @@
 #include "Labyrinth.h"
+#include "set.h"
+using namespace std;
 
-bool isPathToFreedom(MazeCell* start, const std::string& moves) {
-    /* TODO: Delete this comment and the remaining lines, then implement this function. */
-    (void) start;
-    (void) moves;
-    return false;
+bool isPathToFreedom(MazeCell* start, const string& moves) {
+    Set<Item> magicalItems;
+
+    for (int i = 0; i < moves.size(); i++) {
+        if (start->whatsHere != Item::NOTHING) {
+            magicalItems.add(start->whatsHere);
+        }
+
+        if (moves[i] == 'E') {
+            if (start->east != nullptr)
+                start = start->east;
+            else
+                return false;
+        }
+        else if (moves[i] == 'N') {
+            if (start->north != nullptr)
+                start = start->north;
+            else
+                return false;
+        }
+        else if (moves[i] == 'S') {
+            if (start->south != nullptr)
+                start = start->south;
+            else
+                return false;
+        }
+        else if (moves[i] == 'W') {
+            if (start->west != nullptr)
+                start = start->west;
+            else
+                return false;
+        }
+    }
+    if (start->whatsHere != Item::NOTHING) {
+        magicalItems.add(start->whatsHere);
+    }
+    return magicalItems.size() == 3;
 }
