e4bfa1b9f23b3ed48b0db40638075563dec43e88
Eason Lu <aelnosu@outlook.com>
2023-11-02 15:26:40 -0700

    4

--- Main.cpp
+++ Main.cpp
@@ -38,6 +38,6 @@ void generateCombinations(const std::string &characters, int length, std::string
 }
 int main()
 {
-    generateCombinations("NSEW", 15, "");
+    generateCombinations("NSEW", 4, "");
     return 0;
 }
